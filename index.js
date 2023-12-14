12b57c1d2e01cd8b1c91c894d6d64aab28955b4c
miriam <miriamwangui525@gmail.com>
2023-12-14 12:29:21 +0300

    first funstions in javascript

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+    const hqLocation = 42;
+    return Math.abs(pickupLocation - hqLocation);
+  }
+  
+  function distanceFromHqInFeet(pickupLocation) {
+    const feetPerBlock = 264;
+    return distanceFromHqInBlocks(pickupLocation) * feetPerBlock;
+  }
+  
+  function distanceTravelledInFeet(start, destination) {
+    const feetPerBlock = 264;
+    return Math.abs(destination - start) * feetPerBlock;
+  }
+  
+  const pickupLocation = 50;
+  const startBlock = 34;
+  const destinationBlock = 38;
+  
+  console.log(distanceFromHqInBlocks(pickupLocation)); 
+  console.log(distanceFromHqInFeet(pickupLocation));  
+  console.log(distanceTravelledInFeet(startBlock, destinationBlock)); 
+
+  
+
+
+function calculatesFarePrice(start, destination) {
+    const feetPerBlock = 264;
+    const distance = Math.abs(destination - start) * feetPerBlock;
+  
+    if (distance <= 400) {
+      return 0;
+    }else if (distance <= 2000) {
+      return (distance - 400) * 0.02;
+    } else if (distance <= 2500) {
+      return 25;
+    } else {
+      return 'cannot travel that far';
+    }
+  }
+  
+ 
+  
